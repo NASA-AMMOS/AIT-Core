30bedfd8f4af81c5bb44b8dd3bd9c794eb1bf2cf
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2016-05-06 17:11:24 -0700

    Issue #43 - Initial tests for val.YAMLProcessor

---
+++ bliss/test/test_val.py
@@ -0,0 +1,65 @@
+import os
+
+import bliss
+import mock
+import nose
+
+class TestYAMLProcessor(object):
+    def test_yamlprocess_init(self):
+        yp = bliss.val.YAMLProcessor()
+        assert yp.loaded == False
+        assert yp.data == []
+        assert yp.doclines == []
+        assert yp._clean
+        assert yp.ymlfile is None
+
+    @mock.patch('bliss.val.YAMLProcessor.load')
+    def test_ymlfile_setter(self, yaml_load_mock):
+        yp = bliss.val.YAMLProcessor()
+        assert yaml_load_mock.call_count == 0 
+        yp.ymlfile = 'something that is not None'
+        assert yaml_load_mock.call_count == 1 
+
+    @mock.patch('bliss.val.YAMLProcessor.process')
+    def test_yaml_load_with_clean(self, process_mock):
+        yp = bliss.val.YAMLProcessor()
+        yp.load()
+        assert process_mock.called
+        assert yp.loaded
+
+    def test_yaml_load_without_clean(self):
+        yaml_docs = (
+            '---\n'
+            'a: hello\n'
+            '---\n'
+            'b: goodbye\n'
+        )
+
+        with open('/tmp/test.yaml', 'wb') as out:
+            out.write(yaml_docs)
+
+        yp = bliss.val.YAMLProcessor(clean=False)
+        yp.load('/tmp/test.yaml')
+        assert len(yp.data) == 2
+        assert yp.data[0]['a'] == 'hello'
+        assert yp.data[1]['b'] == 'goodbye'
+
+        os.remove('/tmp/test.yaml')
+
+    def test_invalid_yaml_load(self):
+        yaml_docs = """
+        ---
+        a: these newlines and white space break stuff
+        ---
+        b: processing wont even get here
+        """
+        with open('/tmp/test.yaml', 'wb') as out:
+            out.write(yaml_docs)
+
+        yp = bliss.val.YAMLProcessor(clean=False)
+        nose.tools.assert_raises(
+            bliss.val.YAMLError,
+            yp.load, '/tmp/test.yaml'
+        )
+
+        os.remove('/tmp/test.yaml')
