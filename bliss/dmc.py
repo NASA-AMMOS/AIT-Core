678a529d08314d95f616a1db47236bd65b77e2f3
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2016-07-13 10:35:30 -0700

    Fix datetime references in dmc leap second calculation

--- bliss/dmc.py
+++ bliss/dmc.py
@@ -111,44 +111,44 @@ def getUTCtoGPSLeapSeconds(timestamp=None):
     if timestamp is None:
         timestamp = datetime.datetime.utcnow()
 
-    if timestamp < datetime(1980, 1, 6):
+    if timestamp < datetime.datetime(1980, 1, 6):
         e = "The timestamp date is before the GPS epoch"
         raise ValueError(e)
-    elif datetime(1980, 1, 6) <= timestamp < datetime(1981, 7, 1):
+    elif datetime.datetime(1980, 1, 6) <= timestamp < datetime.datetime(1981, 7, 1):
         return 0
-    elif datetime(1981, 7, 1) <= timestamp < datetime(1982, 7, 1):
+    elif datetime.datetime(1981, 7, 1) <= timestamp < datetime.datetime(1982, 7, 1):
         return 1
-    elif datetime(1982, 7, 1) <= timestamp < datetime(1983, 7, 1):
+    elif datetime.datetime(1982, 7, 1) <= timestamp < datetime.datetime(1983, 7, 1):
         return 2
-    elif datetime(1983, 7, 1) <= timestamp < datetime(1985, 7, 1):
+    elif datetime.datetime(1983, 7, 1) <= timestamp < datetime.datetime(1985, 7, 1):
         return 3
-    elif datetime(1985, 7, 1) <= timestamp < datetime(1988, 1, 1):
+    elif datetime.datetime(1985, 7, 1) <= timestamp < datetime.datetime(1988, 1, 1):
         return 4
-    elif datetime(1988, 1, 1) <= timestamp < datetime(1990, 1, 1):
+    elif datetime.datetime(1988, 1, 1) <= timestamp < datetime.datetime(1990, 1, 1):
         return 5
-    elif datetime(1990, 1, 1) <= timestamp < datetime(1991, 1, 1):
+    elif datetime.datetime(1990, 1, 1) <= timestamp < datetime.datetime(1991, 1, 1):
         return 6
-    elif datetime(1991, 1, 1) <= timestamp < datetime(1992, 7, 1):
+    elif datetime.datetime(1991, 1, 1) <= timestamp < datetime.datetime(1992, 7, 1):
         return 7
-    elif datetime(1992, 7, 1) <= timestamp < datetime(1993, 7, 1):
+    elif datetime.datetime(1992, 7, 1) <= timestamp < datetime.datetime(1993, 7, 1):
         return 8
-    elif datetime(1993, 7, 1) <= timestamp < datetime(1994, 7, 1):
+    elif datetime.datetime(1993, 7, 1) <= timestamp < datetime.datetime(1994, 7, 1):
         return 9
-    elif datetime(1994, 7, 1) <= timestamp < datetime(1996, 1, 1):
+    elif datetime.datetime(1994, 7, 1) <= timestamp < datetime.datetime(1996, 1, 1):
         return 10
-    elif datetime(1996, 1, 1) <= timestamp < datetime(1997, 7, 1):
+    elif datetime.datetime(1996, 1, 1) <= timestamp < datetime.datetime(1997, 7, 1):
         return 11
-    elif datetime(1997, 7, 1) <= timestamp < datetime(1999, 1, 1):
+    elif datetime.datetime(1997, 7, 1) <= timestamp < datetime.datetime(1999, 1, 1):
         return 12
-    elif datetime(1999, 1, 1) <= timestamp < datetime(2006, 1, 1):
+    elif datetime.datetime(1999, 1, 1) <= timestamp < datetime.datetime(2006, 1, 1):
         return 13
-    elif datetime(2006, 1, 1) <= timestamp < datetime(2009, 1, 1):
+    elif datetime.datetime(2006, 1, 1) <= timestamp < datetime.datetime(2009, 1, 1):
         return 14
-    elif datetime(2009, 1, 1) <= timestamp < datetime(2012, 7, 1):
+    elif datetime.datetime(2009, 1, 1) <= timestamp < datetime.datetime(2012, 7, 1):
         return 15
-    elif datetime(2012, 7, 1) <= timestamp < datetime(2015, 7, 1):
+    elif datetime.datetime(2012, 7, 1) <= timestamp < datetime.datetime(2015, 7, 1):
         return 16
-    elif timestamp >= datetime(2015, 7, 1):
+    elif timestamp >= datetime.datetime(2015, 7, 1):
         return 17
 
 
