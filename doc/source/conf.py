0507373307de5912613e078e7f302a772fb10373
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2016-07-21 12:43:08 -0700

    Issue #101 - Include __init__ methods in sphinx docs and sort by type by default

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -288,3 +288,15 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+# Sort members by type
+autodoc_member_order = 'groupwise'
+
+# Ensure that the __init__ method gets documented.
+def skip(app, what, name, obj, skip, options):
+    if name == "__init__":
+        return False
+    return skip
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
