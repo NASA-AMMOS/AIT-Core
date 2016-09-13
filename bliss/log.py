3a7bef0bf8b7ca490b7fe6f8c5f9a692443c9362
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2016-09-13 01:44:50 -0700

    cfg.py updates: * removed use of bliss.log module because log.py needs to use bliss.config in order to determine it's hostname/logger name. We could load log.py before the config and read in the necessary information, but the logging messages in cfg.py don't really serve much purpose beyond the local user anyways. they are still logged, just not beyond the localhost.
    log.py updates:
    * get hostname from bliss.config - fixes a bug with hardcoded hostname, which was incorrectly directing messages to eco-scf.jpl.nasa.gov.
    * get logger name from bliss.config - I'm not exactly sure how oco3-tb reads these log messages, but it appears if the syslog5424_app != 'oco3' (logging.getLgoger('oco3') it ignores it. I think it makes sense for this to be configurable anyways, regardless of whether we can change the way our syslog reads in this data.
    * add new PROGRAM log type
    * add method for easily passing COMMAND and PROGRAM logs:
          bliss.log.command(msg)
          bliss.log.program(msg)
    
    config.yaml updates:
    * add default.logging.name and default.logging.hostname to the config for the new logging information

--- bliss/log.py
+++ bliss/log.py
@@ -19,220 +19,237 @@ import logging.handlers
 
 import bliss
 
-
 NOTICE  = logging.INFO + 1
 COMMAND = logging.INFO + 2
+PROGRAM = logging.INFO + 3
 logging.addLevelName(NOTICE , 'NOTICE' )
 logging.addLevelName(COMMAND, 'COMMAND')
+logging.addLevelName(PROGRAM, 'PROGRAM')
 
 
 class LogFormatter (logging.Formatter):
-  """LogFormatter
+    """LogFormatter
 
-  Log output format is:
+    Log output format is:
 
-    YYYY-MM-DDTHH:MM:SS | levelname | message
+        YYYY-MM-DDTHH:MM:SS | levelname | message
 
-  Where levelname is one of 'critical', 'error', 'warning', 'command',
-  'info', or 'debug'.
-  """
-
-  DATEFMT = "%Y-%m-%dT%H:%M:%S"
-
-  def __init__ (self):
-    """LogFormatter()
-
-    Creates and returns a new LogFormatter.
+    Where levelname is one of 'critical', 'error', 'warning', 'command',
+    'info', or 'debug'.
     """
-    format  = "%(asctime)s | %(levelname)-8s | %(message)s"
-    datefmt = self.DATEFMT
-    logging.Formatter.__init__(self, format, datefmt)
+
+    DATEFMT = "%Y-%m-%dT%H:%M:%S"
+
+    def __init__ (self):
+        """LogFormatter()
+
+        Creates and returns a new LogFormatter.
+        """
+        format  = "%(asctime)s | %(levelname)-8s | %(message)s"
+        datefmt = self.DATEFMT
+        logging.Formatter.__init__(self, format, datefmt)
 
 
-  def formatTime (self, record, datefmt=None):
-    """Return the creation time of the specified LogRecord as formatted
-    text."""
-    if datefmt is None:
-      datefmt = '%Y-%m-%d %H:%M:%S'
+    def formatTime (self, record, datefmt=None):
+        """Return the creation time of the specified LogRecord as formatted
+        text."""
+        if datefmt is None:
+            datefmt = '%Y-%m-%d %H:%M:%S'
 
-    ct = self.converter(record.created)
-    t  = time.strftime(datefmt, ct)
-    s  = '%s.%03d' % (t, record.msecs)
+        ct = self.converter(record.created)
+        t  = time.strftime(datefmt, ct)
+        s  = '%s.%03d' % (t, record.msecs)
 
-    return s
+        return s
 
 
 
 class SysLogFormatter (logging.Formatter):
-  """SysLogFormatter"""
+    """SysLogFormatter"""
 
-  BSD_FMT = '%(asctime)s %(hostname)s %(name)s[%(process)d]: %(message)s'
-  BSD_DATEFMT = '%b %d %H:%M:%S'
+    BSD_FMT = '%(asctime)s %(hostname)s %(name)s[%(process)d]: %(message)s'
+    BSD_DATEFMT = '%b %d %H:%M:%S'
 
-  SYS_DATEFMT = '%Y-%m-%dT%H:%M:%S.%fZ'
-  SYSLOG_FMT = ('1 %(asctime)s %(hostname)s %(name)s %(process)d %(levelname)s - '
-                '%(message)s')
-
-
-  def __init__ (self, bsd=False):
-    """LogFormatter([bsd=False])
-
-    Creates and returns a new SysLogFormatter.  If BSD is True, the
-    sylog message is formatted according to the BSD Syslog Protocol:
-
-      RFC 3164 - The BSD Syslog Protocol
-      http://tools.ietf.org/html/rfc3164
-
-    Otherwise, the syslog message is formatted according to the Syslog
-    Protocol:
-
-      RFC 5424 - The Syslog Protocol
-      http://tools.ietf.org/html/rfc5424
-    """
-    self.bsd      = bsd
-    self.hostname = socket.gethostname()
-
-    if self.bsd is True:
-      format  = self.BSD_FMT
-    else:
-      format  = self.SYSLOG_FMT
-
-    logging.Formatter.__init__(self, format)
+    SYS_DATEFMT = '%Y-%m-%dT%H:%M:%S.%fZ'
+    SYSLOG_FMT = ('1 %(asctime)s %(hostname)s %(name)s %(process)d %(levelname)s - '
+                                '%(message)s')
 
 
-  def format (self, record):
-    """Returns the given LogRecord as formatted text."""
-    record.hostname = self.hostname
-    return logging.Formatter.format(self, record)
+    def __init__ (self, bsd=False):
+        """LogFormatter([bsd=False])
+
+        Creates and returns a new SysLogFormatter.  If BSD is True, the
+        sylog message is formatted according to the BSD Syslog Protocol:
+
+            RFC 3164 - The BSD Syslog Protocol
+            http://tools.ietf.org/html/rfc3164
+
+        Otherwise, the syslog message is formatted according to the Syslog
+        Protocol:
+
+            RFC 5424 - The Syslog Protocol
+            http://tools.ietf.org/html/rfc5424
+        """
+        self.bsd      = bsd
+        self.hostname = socket.gethostname()
+
+        if self.bsd is True:
+            format  = self.BSD_FMT
+        else:
+            format  = self.SYSLOG_FMT
+
+        logging.Formatter.__init__(self, format)
 
 
-  def formatTime (self, record, datefmt=None):
-    """Returns the creation time of the given LogRecord as formatted text.
+    def format (self, record):
+        """Returns the given LogRecord as formatted text."""
+        record.hostname = self.hostname
+        return logging.Formatter.format(self, record)
 
-    NOTE: The datefmt parameter and self.converter (the time
-    conversion method) are ignored.  BSD Syslog Protocol messages
-    always use local time, and by our convention, Syslog Protocol
-    messages use UTC.
-    """
-    if self.bsd:
-      lt_ts = datetime.datetime.fromtimestamp(record.created)
-      ts = lt_ts.strftime(self.BSD_DATEFMT)
-      if ts[4] == '0':
-        ts = ts[0:4] + ' ' + ts[5:]
-    else:
-      utc_ts = datetime.datetime.utcfromtimestamp(record.created)
-      ts     = utc_ts.strftime(self.SYS_DATEFMT)
-    return ts
+
+    def formatTime (self, record, datefmt=None):
+        """Returns the creation time of the given LogRecord as formatted text.
+
+        NOTE: The datefmt parameter and self.converter (the time
+        conversion method) are ignored.  BSD Syslog Protocol messages
+        always use local time, and by our convention, Syslog Protocol
+        messages use UTC.
+        """
+        if self.bsd:
+            lt_ts = datetime.datetime.fromtimestamp(record.created)
+            ts = lt_ts.strftime(self.BSD_DATEFMT)
+            if ts[4] == '0':
+                ts = ts[0:4] + ' ' + ts[5:]
+        else:
+            utc_ts = datetime.datetime.utcfromtimestamp(record.created)
+            ts     = utc_ts.strftime(self.SYS_DATEFMT)
+        return ts
 
 
 
 class SysLogHandler (logging.handlers.SysLogHandler):
-  def __init__(self, address=None, facility=None, socktype=None):
-    self.bsd = False
+    def __init__(self, address=None, facility=None, socktype=None):
+        self.bsd = False
 
-    if address is None:
-      if sys.platform == 'darwin':
-        address  = '/var/run/syslog'
-        self.bsd = True
-      else:
-        address = ('localhost', logging.handlers.SYSLOG_UDP_PORT)
+        if address is None:
+            if sys.platform == 'darwin':
+                address  = '/var/run/syslog'
+                self.bsd = True
+            else:
+                address = ('localhost', logging.handlers.SYSLOG_UDP_PORT)
 
-    if facility is None:
-      facility = logging.handlers.SysLogHandler.LOG_USER
+        if facility is None:
+            facility = logging.handlers.SysLogHandler.LOG_USER
 
-    logging.handlers.SysLogHandler.__init__(self, address, facility, socktype)
+        logging.handlers.SysLogHandler.__init__(self, address, facility, socktype)
 
-    self.priority_map['COMMAND'] = 'notice'
-    self.priority_map['NOTICE']  = 'notice'
+        self.priority_map['NOTICE']  = 'notice'
+        self.priority_map['COMMAND'] = 'notice'
+        self.priority_map['PROGRAM'] = 'notice'
 
-    self.setFormatter( SysLogFormatter(self.bsd) )
+        self.setFormatter( SysLogFormatter(self.bsd) )
 
 class SysLogParser(object):
-  """Parses SysLog into a dictionary. Assumes RFC 5424 - The Syslog Protocol"""
+    """Parses SysLog into a dictionary. Assumes RFC 5424 - The Syslog Protocol"""
 
-  def __init__(self, fmt=None):
-    self.fmt = fmt
+    def __init__(self, fmt=None):
+        self.fmt = fmt
 
-    if not fmt:
-      self.fmt = "<%(pri)s>" + SysLogFormatter.SYSLOG_FMT + "$"
+        if not fmt:
+            self.fmt = "<%(pri)s>" + SysLogFormatter.SYSLOG_FMT + "$"
 
-  def parse(self, msg, fmt=None):
-    if fmt:
-      self.fmt = fmt
+    def parse(self, msg, fmt=None):
+        if fmt:
+            self.fmt = fmt
 
-    regex = get_regex(self.fmt)
-    match = regex.match(msg)
-    return match.groupdict()
+        regex = get_regex(self.fmt)
+        match = regex.match(msg)
+        return match.groupdict()
 
 
 def addHandlers (logger):
-  """Adds handlers to the given Logger."""
-  termlog  = logging.StreamHandler()
-  handlers = [ termlog, SysLogHandler(), SysLogHandler( ('localhost', 2514) ) ]
+    """Adds handlers to the given Logger."""
+    termlog  = logging.StreamHandler()
+    handlers = [ termlog, SysLogHandler(), SysLogHandler( ('localhost', 2514) ) ]
 
-  termlog.setFormatter( LogFormatter() )
+    termlog.setFormatter( LogFormatter() )
 
-  try:
-    hostname = 'eco-scf.jpl.nasa.gov'
+    try:
+        try:
+            hostname = bliss.config.logging.hostname
+        except Exception:
+            hostname = 'localhost'
 
-    # We skip logging to hostname if:
-    #
-    #   1.  The hostname cannot be resolved (socket.gethostbyname()
-    #       throws socket.gaierror), or
-    #   2.  The host is the same as localhost, since that's covered by
-    #       SysLogHandler() above.  That is, we don't want to log
-    #       messages to localhost:514 (syslogd) twice.
-    if socket.getfqdn() != hostname:
-      socket.gethostbyname(hostname)
-      handlers.append( SysLogHandler( (hostname,  514) ) )
+        # We skip logging to hostname if:
+        #
+        #   1.  The hostname cannot be resolved (socket.gethostbyname()
+        #       throws socket.gaierror), or
+        #   2.  The host is the same as localhost, since that's covered by
+        #       SysLogHandler() above.  That is, we don't want to log
+        #       messages to localhost:514 (syslogd) twice.
+        if socket.getfqdn() != hostname:
+            socket.gethostbyname(hostname)
+            handlers.append( SysLogHandler( (hostname,  514) ) )
 
-  except socket.gaierror:
-    pass
+    except socket.gaierror:
+        pass
 
-  for h in handlers:
-    logger.addHandler(h)
+    for h in handlers:
+        logger.addHandler(h)
 
 def get_regex(fmt):
-  """Transforms log format string into regex.
+    """Transforms log format string into regex.
 
-  Formats use the following syntax:
+    Formats use the following syntax:
 
-  .. code-block:: none
+    .. code-block:: none
 
-    :variable - one or more words expected (\w+)
-    @variable - syslog time expected (SYSLOG_TIME_FMT)
-    #variable - any character expected (.+)
+        :variable - one or more words expected (\w+)
+        @variable - syslog time expected (SYSLOG_TIME_FMT)
+        #variable - any character expected (.+)
 
-  """
-  def replace_decimal (match):
-    return "(?P<%s>.+)" % match.group(1)
+    """
+    def replace_decimal (match):
+        return "(?P<%s>.+)" % match.group(1)
 
-  def replace_any (match):
-    return "(?P<%s>.+)" % match.group(1)
+    def replace_any (match):
+        return "(?P<%s>.+)" % match.group(1)
 
 
-  # replace string-like format
-  r = re.sub(r"%\((\w+)(\)s)", replace_any, fmt)
+    # replace string-like format
+    r = re.sub(r"%\((\w+)(\)s)", replace_any, fmt)
 
-  # replace decimal-like format
-  regex = re.compile(re.sub(r"%\((\w+)(\)d)", replace_decimal, r))
-  return regex;
+    # replace decimal-like format
+    regex = re.compile(re.sub(r"%\((\w+)(\)d)", replace_decimal, r))
+    return regex;
 
 def begin ():
-  """Command-line tools should begin logging with bliss.log.begin() to
-  log the command name and arguments.
-  """
-  logger.log(COMMAND, " ".join(sys.argv))
+    """Command-line tools should begin logging with bliss.log.begin() to
+    log the command name and arguments.
+    """
+    logger.log(PROGRAM, " ".join(sys.argv))
 
 
 def end ():
-  """Command-line tools should end logging with bliss.log.end() to
-  log the completion of the command.
-  """
-  logger.log(COMMAND, "done.")
-  logging.shutdown()
+    """Command-line tools should end logging with bliss.log.end() to
+    log the completion of the command.
+    """
+    logger.log(PROGRAM, "done.")
+    logging.shutdown()
 
-logger = logging.getLogger('bliss')
+def command(*args, **kwargs):
+    logger.log(COMMAND, *args, **kwargs)
+
+
+def program(*args, **kwargs):
+    logger.log(PROGRAM, *args, **kwargs)
+
+
+try:
+    logger = logging.getLogger(bliss.config.logging.name)
+except Exception:
+    logger = logging.getLogger('bliss')
+
 crit   = logger.critical
 debug  = logger.debug
 error  = logger.error
