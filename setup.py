NASA-AMMOS/AIT-Core

commit aae6d01a7ed08afb8bde4c9595f240a290fb90c6

Issue #248 - Add entry_points for bin scripts
- Add entry_points for bin scripts to improve user-facing script naming.
Scripts are now accessible with all "_" as "-" and without the ".py"
extension.
- Update scripts so all have a "main()" to call for entry_points
compatibility.

setup.py
@@ -5,16 +5,12 @@ import os
 setup(
     name         = 'bliss-core',
     version      = '0.15.0',
-    packages     = ['bliss.core'],
+    packages     = ['bliss.core', 'bin'],
     author       = 'BLISS-Core Development Team',
     author_email = 'bliss@jpl.nasa.gov',
 
-    namespace_packages   = [ 'bliss' ],
+    namespace_packages   = ['bliss'],
     include_package_data = True,
-
-    scripts = [
-        os.path.join('./bin', f) for f in os.listdir('./bin')
-    ],
 
     package_data = {
         'bliss.core': ['data/*.json']
@@ -42,5 +38,14 @@ setup(
             'mock',
             'pylint'
         ],
+    },
+
+    entry_points = {
+        'console_scripts': [
+            '{}=bin.{}:main'.format(
+                f.split('.')[0].replace('_', '-'),
+                f.split('.')[0])
+            for f in os.listdir('./bin')
+        ]
     }
 )

bin/bliss_bsc_create_handler.py
@@ -25,8 +25,7 @@ Usage:
 from docopt import docopt
 import requests
 
-
-if __name__ == '__main__':
+def main():
     arguments = docopt(__doc__)
 
     host = arguments.pop('--service-host')
@@ -52,3 +51,6 @@ if __name__ == '__main__':
         'http://{}:{}/{}/start'.format(host, port, handler_name),
         data=arguments
     )
+
+if __name__ == '__main__':
+    main()

bin/bliss_bsc_stop_handler.py
@@ -14,7 +14,7 @@ from docopt import docopt
 import requests
 
 
-if __name__ == '__main__':
+def main():
     arguments = docopt(__doc__)
 
     host = arguments.pop('--service-host')
@@ -23,3 +23,6 @@ if __name__ == '__main__':
     handler_name = arguments.pop('<name>')
 
     requests.delete('http://{}:{}/{}/stop'.format(host, port, handler_name))
+
+if __name__ == '__main__':
+    main()

bin/bliss_create_dirs.py
@@ -201,7 +201,7 @@ def createDirStruct(paths, verbose=False):
 
     return True
 
-if __name__ == '__main__':
+def main():
     arguments = docopt(__doc__)
 
     try:
@@ -244,3 +244,6 @@ if __name__ == '__main__':
     except Exception as e:
         print e
         log.error('BLISS Create Directories error: %s' % traceback.format_exc())
+
+if __name__ == '__main__':
+    main()

bin/bliss_dict_writer.py
@@ -30,8 +30,7 @@ import sys
 
 from bliss.core import log, tlm
 
-
-if __name__ == '__main__':
+def main():
     arguments = docopt(__doc__)
 
     # output format for the file
@@ -54,3 +53,6 @@ if __name__ == '__main__':
         writer.writeToCSV(output_path=path)
     else:
         log.error("Invalid <format> specified.")
+
+if __name__ == '__main__':
+    main()

bin/bliss_orbits.py
@@ -83,138 +83,142 @@ import docopt
 import bliss
 
 
-arguments = docopt.docopt(__doc__, version='bliss-orbits 0.1.0')
-predicts  = arguments['predicts']
-actuals   = arguments['actuals']
-filename  = arguments['<filename>']
-outname   = arguments['-o']
-start     = arguments['<start-time>']
-stop      = arguments['<stop-time>']
-today     = start.lower() == 'today'
-date      = None
-doy       = None
+def main():
+    arguments = docopt.docopt(__doc__, version='bliss-orbits 0.1.0')
+    predicts  = arguments['predicts']
+    actuals   = arguments['actuals']
+    filename  = arguments['<filename>']
+    outname   = arguments['-o']
+    start     = arguments['<start-time>']
+    stop      = arguments['<stop-time>']
+    today     = start.lower() == 'today'
+    date      = None
+    doy       = None
 
-if re.match('\d{4}-\d{2}-\d{2}$', start):
-    date = start
+    if re.match('\d{4}-\d{2}-\d{2}$', start):
+        date = start
 
-if re.match('\d{1,3}$', start):
-    doy = start
+    if re.match('\d{1,3}$', start):
+        doy = start
 
-now = datetime.datetime.utcnow()
-day = True
-
-
-# Parse and determine start and stop filter times.
-try:
-    if start and stop:
-        field  = 'Start time'
-        format = 'YYYY-MM-DDTHH:MM:SS'
-        value  = start
-        start  = datetime.datetime.strptime(start, '%Y-%m-%dT%H:%M:%S')
-
-        field  = 'Stop time'
-        value  = stop
-        stop   = datetime.datetime.strptime(stop , '%Y-%m-%dT%H:%M:%S')
-
-        day    = False
-
-    elif today:
-        start = now.replace(hour=0, minute=0, second=0, microsecond=0)
-
-    elif doy:
-        field = 'DOY'
-        start = datetime.datetime.strptime(doy, '%j').replace(year=now.year)
-
-    elif date:
-        format = 'YYYY-MM-DD'
-        field  = 'Date'
-        value  = date
-        start  = datetime.datetime.strptime(date, '%Y-%m-%d')
-
-    else:
-        start = None
-        stop  = None
-        day   = False
-
-    if day:
-        stop = start.replace(hour=23, minute=59, second=59, microsecond=999999)
-
-except ValueError, e:
-    if field == 'DOY':
-        bliss.log.error('DOY "%s" is not a number or in range [0 366].', doy)
-    else:
-        msg = '%s "%s" does not match format %s.'
-        bliss.log.error(msg, field, value, format)
-    bliss.log.end()
-    sys.exit(1)
+    now = datetime.datetime.utcnow()
+    day = True
 
 
-# Ensure output file can be opened for writing.
-if outname:
+    # Parse and determine start and stop filter times.
     try:
-        output = open(outname, 'wt')
-        stdout = False
-    except IOError, e:
-        bliss.log.error('Could not open "%s" for writing.', outname)
-        bliss.log.end()
+        if start and stop:
+            field  = 'Start time'
+            format = 'YYYY-MM-DDTHH:MM:SS'
+            value  = start
+            start  = datetime.datetime.strptime(start, '%Y-%m-%dT%H:%M:%S')
+
+            field  = 'Stop time'
+            value  = stop
+            stop   = datetime.datetime.strptime(stop , '%Y-%m-%dT%H:%M:%S')
+
+            day    = False
+
+        elif today:
+            start = now.replace(hour=0, minute=0, second=0, microsecond=0)
+
+        elif doy:
+            field = 'DOY'
+            start = datetime.datetime.strptime(doy, '%j').replace(year=now.year)
+
+        elif date:
+            format = 'YYYY-MM-DD'
+            field  = 'Date'
+            value  = date
+            start  = datetime.datetime.strptime(date, '%Y-%m-%d')
+
+        else:
+            start = None
+            stop  = None
+            day   = False
+
+        if day:
+            stop = start.replace(hour=23, minute=59, second=59, microsecond=999999)
+
+    except ValueError, e:
+        if field == 'DOY':
+            bliss.core.log.error('DOY "%s" is not a number or in range [0 366].', doy)
+        else:
+            msg = '%s "%s" does not match format %s.'
+            bliss.core.log.error(msg, field, value, format)
+        bliss.core.log.end()
         sys.exit(1)
-else:
-    output = sys.stdout
-    stdout = True
 
 
-# Generate and filter orbits.
-bliss.log.begin()
+    # Ensure output file can be opened for writing.
+    if outname:
+        try:
+            output = open(outname, 'wt')
+            stdout = False
+        except IOError, e:
+            bliss.core.log.error('Could not open "%s" for writing.', outname)
+            bliss.core.log.end()
+            sys.exit(1)
+    else:
+        output = sys.stdout
+        stdout = True
 
-if predicts:
-    bliss.log.info('Reading JSC Pointing World report "%s".', filename)
-    report    = bliss.iss.pointing.WorldReport(filename)
-    path      = report.path
-elif actuals:
-    bliss.log.info('Reading ISS Ephemeris "%s".', filename)
-    report    = bliss.iss.eph.EphemeridesReport(filename)
-    path      = bliss.iss.eph.toGroundPath(report.ephemerides)
 
-crossings = list( path.crossings() )
+    # Generate and filter orbits.
+    bliss.core.log.begin()
 
-bliss.log.info('Found %6d total orbits.', len(crossings))
+    if predicts:
+        bliss.core.log.info('Reading JSC Pointing World report "%s".', filename)
+        report    = bliss.iss.pointing.WorldReport(filename)
+        path      = report.path
+    elif actuals:
+        bliss.core.log.info('Reading ISS Ephemeris "%s".', filename)
+        report    = bliss.iss.eph.EphemeridesReport(filename)
+        path      = bliss.iss.eph.toGroundPath(report.ephemerides)
 
-if start is None and stop is None:
-    filtered = enumerate(crossings)
-else:
-    filtered = [ (orbit, c) for (orbit, c) in enumerate(crossings)
-                     if c.first.time >= start and c.first.time < stop ]
-    msg      = 'Found %6d total orbits for ' % len(filtered)
+    crossings = list( path.crossings() )
 
-    if today:
-        msg += 'today.'
-        bliss.log.info(msg)
-    elif doy:
-        msg += 'DOY %03d.'
-        bliss.log.info(msg, int(doy))
-    elif date:
-        msg += 'for date %s.'
-        bliss.log.info(msg, start.strftime('%Y-%m-%d'))
-    elif start and stop:
-        msg += 'for date/time range [%s, %s)'
-        bliss.log.info(msg, start.isoformat(), stop.isoformat())
+    bliss.core.log.info('Found %6d total orbits.', len(crossings))
 
-if stdout:
-    print
-else:
-    bliss.log.info('Writing orbits to "%s".' % outname)
+    if start is None and stop is None:
+        filtered = enumerate(crossings)
+    else:
+        filtered = [ (orbit, c) for (orbit, c) in enumerate(crossings)
+                         if c.first.time >= start and c.first.time < stop ]
+        msg      = 'Found %6d total orbits for ' % len(filtered)
 
-for (orbit, c) in filtered:
-    begin = c.first.time + datetime.timedelta(seconds=1)
-    begin = begin.strftime('%Y-%m-%dT%H:%M:%S')
-    end   = c.last.time.strftime('%Y-%m-%dT%H:%M:%S')
-    delta = (c.last.time - c.first.time).seconds / 60.
-    output.write('%s\t%s\t%f\t%06d\n' % (begin, end, delta, orbit))
+        if today:
+            msg += 'today.'
+            bliss.core.log.info(msg)
+        elif doy:
+            msg += 'DOY %03d.'
+            bliss.core.log.info(msg, int(doy))
+        elif date:
+            msg += 'for date %s.'
+            bliss.core.log.info(msg, start.strftime('%Y-%m-%d'))
+        elif start and stop:
+            msg += 'for date/time range [%s, %s)'
+            bliss.core.log.info(msg, start.isoformat(), stop.isoformat())
 
-if stdout:
-    print
+    if stdout:
+        print
+    else:
+        bliss.core.log.info('Writing orbits to "%s".' % outname)
 
-if output != sys.stdout:
-    output.close()
+    for (orbit, c) in filtered:
+        begin = c.first.time + datetime.timedelta(seconds=1)
+        begin = begin.strftime('%Y-%m-%dT%H:%M:%S')
+        end   = c.last.time.strftime('%Y-%m-%dT%H:%M:%S')
+        delta = (c.last.time - c.first.time).seconds / 60.
+        output.write('%s\t%s\t%f\t%06d\n' % (begin, end, delta, orbit))
 
-bliss.log.end()
+    if stdout:
+        print
+
+    if output != sys.stdout:
+        output.close()
+
+    bliss.core.log.end()
+
+if __name__ == '__main__':
+    main()

bin/bliss_tlm_send.py
@@ -28,60 +28,64 @@ defaults = {
 }
 
 
-try:
-    if '--help' in sys.argv:
-        gds.usage(exit=True)
+def main():
+    try:
+        if '--help' in sys.argv:
+            gds.usage(exit=True)
 
-    log.begin()
-    options, args = gds.parseArgs(sys.argv[1:], defaults)
+        log.begin()
+        options, args = gds.parseArgs(sys.argv[1:], defaults)
 
-    if len(args) == 0:
-        gds.usage(exit=True)
-    else:
-        filename = args[0]
+        if len(args) == 0:
+            gds.usage(exit=True)
+        else:
+            filename = args[0]
 
-    host    = 'localhost'
-    port    = options['port']
-    sock    = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    verbose = options['verbose']
+        host    = 'localhost'
+        port    = options['port']
+        sock    = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        verbose = options['verbose']
 
-    if not verbose:
-        log.info('Will only report every 10 telemetry packets')
-        log.info('Will only report long telemetry send delays')
+        if not verbose:
+            log.info('Will only report every 10 telemetry packets')
+            log.info('Will only report long telemetry send delays')
 
-    with pcap.open(filename, 'r') as stream:
-        npackets = 0
-        prev_ts  = None
+        with pcap.open(filename, 'r') as stream:
+            npackets = 0
+            prev_ts  = None
 
-        for header, packet in stream:
-            if prev_ts is None:
-                prev_ts = header.ts
+            for header, packet in stream:
+                if prev_ts is None:
+                    prev_ts = header.ts
 
-            delay = header.ts - prev_ts
+                delay = header.ts - prev_ts
 
-            if delay >= 2:
-                log.info('Next telemetry in %1.2f seconds' % delay)
+                if delay >= 2:
+                    log.info('Next telemetry in %1.2f seconds' % delay)
 
-            time.sleep(delay)
+                time.sleep(delay)
 
-            nbytes = len(packet)
+                nbytes = len(packet)
 
-            if npackets == 0:
-                log.info('Sent first telemetry packet (%d bytes)' % nbytes)
-            elif verbose:
-                log.info('Sent telemetry (%d bytes)' % nbytes)
-            elif npackets % 10 == 0:
-                log.info('Sent 10 telemetry packets')
+                if npackets == 0:
+                    log.info('Sent first telemetry packet (%d bytes)' % nbytes)
+                elif verbose:
+                    log.info('Sent telemetry (%d bytes)' % nbytes)
+                elif npackets % 10 == 0:
+                    log.info('Sent 10 telemetry packets')
 
-            sock.sendto(packet, (host, port))
+                sock.sendto(packet, (host, port))
 
-            npackets += 1
-            prev_ts   = header.ts
+                npackets += 1
+                prev_ts   = header.ts
 
-except KeyboardInterrupt:
-  log.info('Received Ctrl-C.  Stopping telemetry stream.')
+    except KeyboardInterrupt:
+      log.info('Received Ctrl-C.  Stopping telemetry stream.')
 
-except Exception as e:
-  log.error('TLM send error: %s' % str(e))
+    except Exception as e:
+      log.error('TLM send error: %s' % str(e))
 
-log.end()
+    log.end()
+
+if __name__ == '__main__':
+    main()

commit 62d22eb52590ff36aef71a7c023b1e7a4f145e97

Bug Fix: 'Duplicate entry point' error in setup.py
Fixes a bug likely introduced in commit aae6d01, Issue #248 - Add
entry_points for bin scripts.

Whenever I tried to run any BLISS setup.py command, I would receive
the following error:

    error in bliss-core setup command: ('Duplicate entry point',
    'console_scripts', '--init--')

I'm not really familiar with setup.py entry_points or console_scripts,
but after inspecting my checked-out bin/ directory, I found both an
(empty) __init__.py *and* an __init__.pyc.  Since console_scripts in
setup.py are enumerated via:

    for f in os.listdir('./bin')

It seemed prudent to limit bin scripts to only those files in the /bin
directory that have a Python (.py) extension, i.e.:

    for f in os.listdir('./bin') if f.endswith('.py')

With that change, setup.py once again works.

setup.py
@@ -45,7 +45,7 @@ setup(
             '{}=bin.{}:main'.format(
                 f.split('.')[0].replace('_', '-'),
                 f.split('.')[0])
-            for f in os.listdir('./bin')
+            for f in os.listdir('./bin') if f.endswith('.py')
         ]
     }
 )