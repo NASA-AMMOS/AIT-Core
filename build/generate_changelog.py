bd1c054273b16317cbc09f4f10e7a2b88841f106
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2017-08-15 07:49:03 -0700

    Issue #399 - Update changelog gen script to ignore non-resolved tickets
    The changelog generation script now properly ignores tickets without an
    assigned milestone and tickets marked as duplicates / wontfix.

--- build/generate_changelog.py
+++ build/generate_changelog.py
@@ -39,11 +39,20 @@ while True:
 
 issues = []
 for issue in raw_issues:
+    # If the issue is a pull request, skip it
     if 'pull_request' in issue:
         continue
+    # If the issue isn't closed, skip it
     elif issue['closed_at'] == 'null' or issue['state'] != 'closed':
         continue
+    # If the issue was closed after the end time parameter, skip it
     elif args['end_time'] < datetime.datetime.strptime(issue['closed_at'], '%Y-%m-%dT%H:%M:%SZ'):
+        continue
+    # If the issue has a label of `wontfix` or `duplicate`, skip it
+    elif issue['labels'] and len([l for l in issue['labels'] if l['name'] in ['resolution-wontfix', 'resolution-duplicate']]) != 0:
+        continue
+    # If the issue has no milestone, skip it
+    elif not issue['milestone']:
         continue
 
     issues.append(issue)
