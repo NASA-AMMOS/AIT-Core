66c5a62ac811717bdfc89231ff6f8af39cebf2b4
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2017-08-24 17:49:53 -0700

    Issue #404 - update _datapaths property to gracefully handle no paths
    * surround in try/catch and also look for other names for the paths (`PATH_KEYS`)
    * move set of possibly 'directory' names out as a constant `PATH_KEYS`

--- bliss/core/cfg.py
+++ bliss/core/cfg.py
@@ -26,6 +26,8 @@ DEFAULT_PATH_VARS = {
     'doy' : time.strftime('%j', time.gmtime())
 }
 
+PATH_KEYS = 'directory', 'file', 'filename', 'path', 'pathname'
+
 def expandConfigPaths (config, prefix=None, datetime=None, pathvars=None, parameter_key='', *keys):
     """Updates all relative configuration paths in dictionary config,
     which contain a key in keys, by prepending prefix.
@@ -36,7 +38,7 @@ def expandConfigPaths (config, prefix=None, datetime=None, pathvars=None, parame
     See util.expandPath().
     """
     if len(keys) == 0:
-        keys = 'directory', 'file', 'filename', 'path', 'pathname'
+        keys = PATH_KEYS
 
     for name, value in config.items():
         if name in keys and type(name) is str:
@@ -302,9 +304,14 @@ class BlissConfig (object):
     def _datapaths(self):
         """Returns a simple key-value map for easy access to data paths"""
         paths = { }
-        data = self._config['data']
-        for k in data:
-            paths[k] = data[k]['path']
+        try:
+            data = self._config['data']
+            for k in data:
+                for pathkey in PATH_KEYS:
+                    if pathkey in data[k]:
+                        paths[k] = data[k][pathkey]
+        except KeyError:
+            pass
 
         return paths
 
