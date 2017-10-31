6873e94097c8936b062e84418c84e29d0a926d35
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2017-10-31 08:18:17 -0700

    Prep for 0.26.0 release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import os
 
 setup(
     name         = 'bliss-core',
-    version      = '0.25.0',
+    version      = '0.26.0',
     packages     = find_packages(exclude=['tests']),
     author       = 'BLISS-Core Development Team',
     author_email = 'bliss@jpl.nasa.gov',
