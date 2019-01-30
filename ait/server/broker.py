adc83c539cdbe5da8ae8d918e028b2c895f67545
Anna Waldron <anna.waldron@jpl.nasa.gov>
2019-01-30 12:02:26 -0800

    GUI Issue #74 - Cleanup of broker code

--- ait/server/broker.py
+++ ait/server/broker.py
@@ -43,7 +43,7 @@ class AitBroker(object):
             zmq.proxy(frontend, backend)
 
         except Exception as e:
-            log.error('ZeroMQ Error: %s' % e)
+            log.error('ZeroMQ Error: {}'.format(e))
             raise(e)
 
         finally:
@@ -52,32 +52,32 @@ class AitBroker(object):
             self.context.term()
 
     def load_streams(self):
-        common_err_msg = 'No valid %s telemetry stream configurations found. '
+        common_err_msg = 'No valid {} telemetry stream configurations found. '
         specific_err_msg = {'inbound': 'No telemetry will be received (or displayed).',
                             'outbound': 'No telemetry will be published.'}
         err_msgs = {}
 
         for stream_type in ['inbound', 'outbound']:
-            err_msgs[stream_type] = common_err_msg % stream_type + specific_err_msg[stream_type]
-            streams = ait.config.get('server.%s-streams' % stream_type)
+            err_msgs[stream_type] = common_err_msg.format(stream_type) + specific_err_msg[stream_type]
+            streams = ait.config.get('server.{}-streams'.format(stream_type))
 
             if streams is None:
-                msg = cfg.AitConfigMissing('server.%s-streams' % stream_type).args[0]
-                log.error(msg + '  ' + err_msgs[stream_type])
+                log.warn(err_msgs[stream_type])
             else:
                 for index, s in enumerate(streams):
                     try:
-                        config_path = 'server.%s-streams[%d].stream' % (stream_type, index)
-                        config = cfg.AitConfig(config=s).get('stream')
-                        strm = self.create_stream(config, config_path, stream_type)
+                        strm = self.create_stream(s['stream'], stream_type)
                         if stream_type == 'inbound':
                             self.inbound_streams.append(strm)
                         elif stream_type == 'outbound':
                             self.outbound_streams.append(strm)
-                        log.info('Added %s stream %s' % (stream_type, strm))
+                        log.info('Added {} stream {}'.format(stream_type, strm))
                     except Exception:
                         exc_type, value, tb = sys.exc_info()
-                        log.error('%s creating stream at %s: %s' % (exc_type, config_path, value))
+                        log.error('{} creating {} stream {}: {}'.format(exc_type,
+                                                                        stream_type,
+                                                                        index,
+                                                                        value))
 
         if not self.inbound_streams:
             log.warn(err_msgs['inbound'])
@@ -85,13 +85,12 @@ class AitBroker(object):
         if not self.outbound_streams:
             log.warn(err_msgs['outbound'])
 
-    def create_stream(self, config, config_path, stream_type):
+    def create_stream(self, config, stream_type):
         """
         Creates a stream from its config.
 
         Params:
             config:       stream configuration as read by ait.config
-            config_path:  string path to stream's yaml config
             stream_type:  either 'inbound' or 'outbound'
         Returns:
             stream:       a Stream
@@ -102,13 +101,11 @@ class AitBroker(object):
             raise ValueError('Stream type must be \'inbound\' or \'outbound\'.')
 
         if config is None:
-            msg = cfg.AitConfigMissing(config_path).args[0]
-            raise ValueError(msg)
+            raise ValueError('No stream config to create stream from.')
 
         name = config.get('name', None)
         if name is None:
-            msg = cfg.AitConfigMissing(config_path + '.name').args[0]
-            raise ValueError(msg)
+            raise(cfg.AitConfigMissing(stream_type + ' stream name'))
         if name in [x.name for x in (self.outbound_streams +
                                      self.inbound_streams +
                                      self.plugins)]:
@@ -116,22 +113,18 @@ class AitBroker(object):
 
         stream_input = config.get('input', None)
         if stream_input is None:
-            msg = cfg.AitConfigMissing(config_path + '.input').args[0]
-            raise ValueError(msg)
+            raise(cfg.AitConfigMissing(stream_type + ' stream input'))
 
         stream_handlers = [ ]
-        handler_cfg_list = cfg.AitConfig(config=config).get('handlers')
-        if handler_cfg_list:
-            for handler in handler_cfg_list:
-                try:
-                    hndlr = self.create_handler(handler,
-                                                config_path + '.handlers')
-                except Exception as e:
-                    raise(e)
-
+        if config['handlers']:
+            for handler in config['handlers']:
+                hndlr = self.create_handler(handler)
                 stream_handlers.append(hndlr)
-                log.info('Created handler %s for stream %s'
-                         % (type(hndlr).__name__, name))
+                log.info('Created handler {} for stream {}'.format(type(hndlr).__name__,
+                                                                   name))
+        else:
+            log.warn('No handlers specified for {} stream {}'.format(stream_type,
+                                                                     name))
 
         return Stream(name,
                       stream_input,
@@ -140,18 +133,15 @@ class AitBroker(object):
                                 'XSUB_URL': self.XSUB_URL,
                                 'XPUB_URL': self.XPUB_URL})
 
-    def create_handler(self, config, config_path):
+    def create_handler(self, config):
         """
         Creates a handler from its config.
 
         Params:
             config:      handler config
-            config_path: config path of stream
         """
-        print(config)
         if config is None:
-            msg = cfg.AitConfigMissing(config_path).args[0]
-            raise ValueError(msg)
+            raise ValueError('No handler config to create handler from.')
 
         # check if input/output types specified
         if type(config) == str:
@@ -164,12 +154,9 @@ class AitBroker(object):
 
         # try to create handler
         class_name = handler_name.title().replace('_', '')
-        try:
-            module = import_module('ait.server.handlers.%s' % handler_name)
-            handler_class = getattr(module, class_name)
-            instance = handler_class(input_type, output_type)
-        except Exception as e:
-            raise(e)
+        module = import_module('ait.server.handlers.{}'.format(handler_name))
+        handler_class = getattr(module, class_name)
+        instance = handler_class(input_type, output_type)
 
         return instance
 
@@ -183,7 +170,7 @@ class AitBroker(object):
 
     def subscribe(self, subscriber, publisher):
         subscriber.sub.setsockopt(zmq.SUBSCRIBE, str(publisher))
-        log.info('Subscribed %s to topic %s' % (subscriber, publisher))
+        log.info('Subscribed {} to topic {}'.format(subscriber, publisher))
 
     def get_stream(self, name):
         return next((strm
@@ -195,43 +182,38 @@ class AitBroker(object):
         plugins = ait.config.get('server.plugins')
 
         if plugins is None:
-            log.warn(cfg.AitConfigMissing('server.plugins').args[0])
+            log.warn('No plugins specified in config.')
         else:
             for index, p in enumerate(plugins):
                 try:
-                    config_path = 'server.plugins[%d]' % (index)
-                    config = cfg.AitConfig(config=p).get('plugin')
-                    plugin = self.create_plugin(config, config_path)
+                    plugin = self.create_plugin(p['plugin'])
                     self.plugins.append(plugin)
-                    log.info('Added plugin %s' % (plugin))
+                    log.info('Added plugin {}'.format(plugin))
                 except Exception:
                     exc_type, value, tb = sys.exc_info()
-                    log.error('%s creating plugin at %s: %s'
-                               % (exc_type, config_path, value))
+                    log.error('{} creating plugin {}: {}'.format(exc_type,
+                                                                 index,
+                                                                 value))
+            if not self.plugins:
+                log.warn('No valid plugin configurations found. No plugins will be added.')
 
-        if not self.plugins:
-            log.warn('No valid plugin configurations found. No plugins will be added.')
-
-    def create_plugin(self, config, config_path):
+    def create_plugin(self, config):
         """
         Creates a plugin from its config.
 
         Params:
             config:       plugin configuration as read by ait.config
-            config_path:  string path to plugin's yaml config
         Returns:
             plugin:       a Plugin
         Raises:
             ValueError:   if any of the required config values are missing
         """
         if config is None:
-            msg = cfg.AitConfigMissing(config_path).args[0]
-            raise ValueError(msg)
+            raise ValueError('No plugin config to create plugin from.')
 
         name = config.get('name', None)
         if name is None:
-            msg = cfg.AitConfigMissing(config_path + '.name').args[0]
-            raise ValueError(msg)
+            raise(cfg.AitConfigMissing('plugin name'))
 
         class_name = name.title().replace('_', '')
         if class_name in [x.name for x in (self.outbound_streams +
@@ -241,21 +223,16 @@ class AitBroker(object):
 
         plugin_inputs = config.get('inputs', None)
         if plugin_inputs is None:
-            msg = cfg.AitConfigMissing(config_path + '.inputs').args[0]
-            log.warn(msg)
+            log.warn('No plugin inputs specified for {}'.format(name))
             plugin_inputs = [ ]
 
         # try to create plugin
-        try:
-            module = import_module('ait.server.plugins.%s' % name)
-            plugin_class = getattr(module, class_name)
-            instance = plugin_class(plugin_inputs,
-                                    zmq_args={'context': self.context,
-                                              'XSUB_URL': self.XSUB_URL,
-                                              'XPUB_URL': self.XPUB_URL})
-        except Exception as e:
-            print(traceback.format_exc())
-            raise(e)
+        module = import_module('ait.server.plugins.{}'.format(name))
+        plugin_class = getattr(module, class_name)
+        instance = plugin_class(plugin_inputs,
+                                zmq_args={'context': self.context,
+                                          'XSUB_URL': self.XSUB_URL,
+                                          'XPUB_URL': self.XPUB_URL})
 
         return instance
 
