59d71e26dc6f60764b03919bfb463cda562f50b2
Anna Waldron <anna.waldron@jpl.nasa.gov>
2019-02-21 14:56:00 -0800

    GUI Issue #75 - More updates to unit tests

--- ait/server/test/test_client.py
+++
@@ -1,8 +0,0 @@
-from ait.server.client import Client
-from nose.tools import *
-import mock
-import zmq
-import ait
-
-
-
--- ait/server/test/test_stream.py
+++ ait/server/test/test_stream.py
@@ -1,32 +1,38 @@
-from ait.server.stream import Stream
-from ait.server.handlers.ccsds_packet_handler import CcsdsPacketHandler
+from ait.server.broker import AitBroker
+from ait.server.stream import ZMQInputStream
+from ait.server.handlers.example_handler import ExampleHandler
 from nose.tools import *
 import mock
-import zmq
+import zmq.green
 import ait
 
 
 class TestStream(object):
-    stream = Stream('some_stream',
-                    'input_stream',
-                    [CcsdsPacketHandler(int, output_type=str)])
+    broker = AitBroker()
+    stream = ZMQInputStream('some_stream',
+                            'input_stream',
+                            [ExampleHandler(input_type=int,
+                                            output_type=str)],
+                            zmq_args={'zmq_context': broker.context})
 
     def setUp(self):
-        self.stream.handlers = [CcsdsPacketHandler(int, output_type=str)]
+        self.stream.handlers = [ExampleHandler(input_type=int,
+                                               output_type=str)]
 
     def test_stream_creation(self):
         assert self.stream.name is 'some_stream'
         assert self.stream.input_ is 'input_stream'
         assert len(self.stream.handlers) == 1
-        assert type(self.stream.handlers[0]) == CcsdsPacketHandler
-        assert self.stream.context == ait.broker.context
-        assert type(self.stream.pub) == zmq.sugar.socket.Socket
-        assert type(self.stream.sub) == zmq.sugar.socket.Socket
+        assert type(self.stream.handlers[0]) == ExampleHandler
+        assert self.stream.context == self.broker.context
+        print(type(self.stream.pub))
+        assert type(self.stream.pub) == zmq.green.core._Socket
+        assert type(self.stream.sub) == zmq.green.core._Socket
 
     def test_repr(self):
-        assert self.stream.__repr__() == '<Stream name=some_stream>'
+        assert self.stream.__repr__() == '<<class \'ait.server.stream.ZMQInputStream\'> name=some_stream>'
 
-    @mock.patch.object(ait.server.handlers.ccsds_packet_handler.CcsdsPacketHandler, 'execute_handler')
+    @mock.patch.object(ExampleHandler, 'execute_handler')
     def test_process(self, execute_handler_mock):
         self.stream.process('input_data')
         execute_handler_mock.assert_called_with('input_data')
@@ -35,31 +41,19 @@ class TestStream(object):
         assert self.stream.valid_workflow() is True
 
     def test_valid_workflow_more_handlers(self):
-        self.stream.handlers.append(CcsdsPacketHandler(str))
+        self.stream.handlers.append(ExampleHandler(input_type=str))
         assert self.stream.valid_workflow() is True
 
     def test_invalid_workflow_more_handlers(self):
-        self.stream.handlers.append(CcsdsPacketHandler(int))
+        self.stream.handlers.append(ExampleHandler(input_type=int))
         assert self.stream.valid_workflow() is False
-
-    def test_type(self):
-        # not registered with broker
-        assert self.stream.type == 'Stream'
-
-        # in broker inbound_streams
-        ait.broker.inbound_streams = [self.stream]
-        assert self.stream.type == 'Inbound Stream'
-
-        # in broker outbound_streams
-        ait.broker.inbound_streams = [ ]
-        ait.broker.outbound_streams = [self.stream]
-        assert self.stream.type == 'Outbound Stream'
 
     def test_stream_creation_invalid_workflow(self):
         with assert_raises_regexp(ValueError,
                                   'Sequential workflow inputs and outputs ' +
                                   'are not compatible. Workflow is invalid.'):
-            Stream('some_stream',
-                   'input_stream',
-                   [CcsdsPacketHandler(int, output_type=str),
-                    CcsdsPacketHandler(int)])
+            ZMQInputStream('some_stream',
+                           'input_stream',
+                           [ExampleHandler(input_type=int, output_type=str),
+                            ExampleHandler(input_type=int)],
+                           zmq_args={'zmq_context': self.broker.context})
