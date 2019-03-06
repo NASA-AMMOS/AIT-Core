02237ae87dbf1899ea9f60042516964f1d38fde7
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2019-03-06 15:01:44 -0800

    Add telem limit monitor plugin

--- ait/core/server/plugin.py
+++ ait/core/server/plugin.py
@@ -5,7 +5,8 @@ import re
 import gevent
 import gevent.monkey; gevent.monkey.patch_all()
 
-from ait.core import tlm, log, db
+import ait.core
+from ait.core import  db, limits, log, notify, tlm
 from client import ZMQInputClient
 
 
@@ -72,3 +73,66 @@ class DataArchive(Plugin):
         finally:
             dbconn.close()
             log.info('Telemetry data archiving terminated')
+
+
+class TelemetryLimitMonitor(Plugin):
+    def __init__(self, inputs, outputs, datastore='ait.core.db.InfluxDBBackend', **kwargs):
+        super(TelemetryLimitMonitor, self).__init__(inputs, outputs, **kwargs)
+
+        self.limit_dict = defaultdict(dict)
+        for k, v in limits.getDefaultDict().iteritems():
+            packet, field = k.split('.')
+            self.limit_dict[packet][field] = v
+
+        self.packet_dict = defaultdict(dict)
+        for k, v in tlm.getDefaultDict().iteritems():
+            self.packet_dict[v.uid] = v
+
+        self.notif_thrshld = ait.config.get('notifications.options.threshold', 1)
+        self.notif_freq = ait.config.get('notifications.options.frequency', float('inf'))
+
+        self.limit_trip_repeats = {}
+        log.info('Starting telemetry limit monitoring')
+
+    def process(self, input_data, topic=None, **kwargs):
+        split = input_data[1:-1].split(',', 1)
+        pkt_id, pkt_data = int(split[0]), split[1]
+        packet = self.packet_dict[pkt_id]
+        decoded = tlm.Packet(packet, data=bytearray(pkt_data))
+
+        if packet.name in self.limit_dict:
+            for field, defn in self.limit_dict[packet.name].iteritems():
+                v = decoded._getattr(field)
+
+                if packet.name not in self.limit_trip_repeats.keys():
+                    self.limit_trip_repeats[packet.name] = {}
+
+                if field not in self.limit_trip_repeats[packet.name].keys():
+                    self.limit_trip_repeats[packet.name][field] = 0
+
+                if defn.error(v):
+                    msg = 'Field {} error out of limit with value {}'.format(field, v)
+                    log.error(msg)
+
+                    self.limit_trip_repeats[packet.name][field] += 1
+                    repeats = self.limit_trip_repeats[packet.name][field]
+
+                    if (repeats == self.notif_thrshld or
+                        (repeats > self.notif_thrshld and
+                        (repeats - self.notif_thrshld) % self.notif_freq == 0)):
+                        notify.trigger_notification('limit-error', msg)
+
+                elif defn.warn(v):
+                    msg = 'Field {} warning out of limit with value {}'.format(field, v)
+                    log.warn(msg)
+
+                    self.limit_trip_repeats[packet.name][field] += 1
+                    repeats = self.limit_trip_repeats[packet.name][field]
+
+                    if (repeats == self.notif_thrshld or
+                        (repeats > self.notif_thrshld and
+                        (repeats - self.notif_thrshld) % self.notif_freq == 0)):
+                        notify.trigger_notification('limit-warn', msg)
+
+                else:
+                    self.limit_trip_repeats[packet.name][field] = 0
