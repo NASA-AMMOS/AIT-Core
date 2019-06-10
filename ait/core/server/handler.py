e0538c7b3c8bc6db170d6beee16a891faa1ecba1
seanlu99 <seanlu99@gmail.com>
2019-06-10 15:55:34 -0700

    Issue #162 - Added more robust handling of APID masking in CCSDSHandler

--- ait/core/server/handler.py
+++ ait/core/server/handler.py
@@ -91,9 +91,11 @@ class CCSDSPacketHandler(Handler):
     """
     This CCSDS handler provides a way to accept multiple packet types on a
     single stream and have them be processed. This handler takes a string of
-    packed binary data (packed by struct) containing CCSDS packet data. It then
-    maps the APID of the packet to a packet name from the config. Using this
-    packet name, it returns the UID and user data field.
+    raw binary data containing CCSDS packet data. It maps the APID of
+    the packet to a packet name from the config, and then uses the packet name
+    to get the UID from the default telemetry dictionary. The user data field
+    is extracted from the raw binary data, and a tuple of the UID and user data
+    field is returned.
     """
     def __init__(self, input_type=None, output_type=None, **kwargs):
         """
@@ -102,7 +104,9 @@ class CCSDSPacketHandler(Handler):
                                      validate handler workflow. Defaults to None.
             output_type:  (optional) Specifies expected output type, used to
                                      validate handler workflow. Defaults to None
-            packet_types: (required) APID value : packet name pairs
+            packet_types: (required) APID value (string) : packet name (string) pairs
+                                     APID value can use 'X' to mask out a bit
+                                     For example, 'XXXXX1011XXX' means only bits 6-9 represent the APID
             packet_secondary_header_length: (optional) Length of secondary header in octets.
                                                        Defaults to 0.
         Raises:
@@ -111,6 +115,8 @@ class CCSDSPacketHandler(Handler):
         super(CCSDSPacketHandler, self).__init__(input_type, output_type)
         self.packet_types = kwargs['packet_types']
         self.packet_secondary_header_length = kwargs.get('packet_secondary_header_length', 0)
+
+        # Check if all packet names in config are in telemetry dictionary
         tlm_dict = tlm.getDefaultDict()
         for packet_name in self.packet_types.values():
             if packet_name not in tlm_dict.keys():
@@ -125,26 +131,63 @@ class CCSDSPacketHandler(Handler):
         Returns:
             tuple of packet UID and packet data field
         """
-        primary_header_length = 6
+
+        # Convert input_data (packed binary data string) into bytearray
         packet = bytearray(input_data)
+
+        # Check if packet length is at least 7 bytes
+        primary_header_length = 6
         if (len(packet) < primary_header_length + 1):
             ait.core.log.info('CCSDSPacketHandler: Received packet length is less than minimum of 7 bytes.')
             return
-        packet_apid = int(binascii.hexlify(packet[0:2]), 16) & 0x07FF
-        if packet_apid not in self.packet_types:
+
+        # Extract APID from packet
+        packet_apid = str(bin(int(binascii.hexlify(packet[0:2]), 16) & 0x07FF))[2:].zfill(11)
+
+        # Check if packet_apid matches with an APID in the config
+        config_apid = ''
+        for i in self.packet_types:
+            if self.comp_apid(packet_apid, i):
+                config_apid = i
+                break
+        if not config_apid:
             msg = 'CCSDSPacketHandler: Packet APID {} not present in config.'.format(packet_apid)
             msg += ' Available packet APIDs are {}'.format(self.packet_types.keys())
             ait.core.log.info(msg)
             return
-        packet_name = self.packet_types[packet_apid]
+
+        # Map APID to packet name in config to get UID from telemetry dictionary
+        packet_name = self.packet_types[config_apid]
         tlm_dict = tlm.getDefaultDict()
         packet_uid = tlm_dict[packet_name].uid
 
+        # Extract user data field from packet
         packet_data_length = int(binascii.hexlify(packet[4:6]), 16) + 1
         if (len(packet) < primary_header_length + packet_data_length):
-            ait.core.log.info('CCSDSPacketHandler: Packet data length is less than stated length in packet primary header.')
+            ait.core.log.info(
+                'CCSDSPacketHandler: Packet data length is less than stated length in packet primary header.')
             return
         udf_length = packet_data_length - self.packet_secondary_header_length
         udf_start = primary_header_length + self.packet_secondary_header_length
         user_data_field = packet[udf_start:udf_start + udf_length + 1]
+
+        ait.core.log.info(packet_apid)
+        ait.core.log.info(user_data_field[0])
+        ait.core.log.info(user_data_field[1])
+        ait.core.log.info(user_data_field[2])
+
         return pickle.dumps((packet_uid, user_data_field), 2)
+
+    def comp_apid(self, server_apid, config_apid):
+        """
+        Params:
+            server_apid:  APID from server
+            config_apid:  APID from config, X represents bit not used
+        Returns:
+            True if server_apid matches config_apid
+            False otherwise
+        """
+        for i in range(11):
+            if config_apid[i] != 'X' and config_apid[i] != server_apid[i]:
+                return False
+        return True
