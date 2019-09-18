28645cc36de0eb4b3132336e56a4ec9de4d94cd3
Bruce Lai <bruce.p.lai@jpl.nasa.gov>
2019-09-18 13:57:12 -0700

    Added cfg import to test_val.py

--- ait/core/test/test_val.py
+++ ait/core/test/test_val.py
@@ -23,7 +23,7 @@ import mock
 import nose
 
 import ait
-from ait.core import cmd, log, tlm, val, util, evr
+from ait.core import cfg, cmd, log, tlm, val, util, evr
 
 
 DATA_PATH = os.path.join(os.path.dirname(__file__), 'testdata', 'val')
