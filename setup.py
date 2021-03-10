fe90bc32cee36595197e1cd3b71a9c070b96038e
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2021-03-10 11:26:04 -0800

    Prep for 0.2.3 release

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ class DevWithGithooks(develop):
 
 setup(
     name         = 'ait-core',
-    version      = '2.2.0',
+    version      = '2.3.0',
     description  = description,
     long_description = long_description,
     long_description_content_type = 'text/x-rst',
