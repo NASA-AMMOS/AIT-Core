fea6217abfb29b1cf4a14037aca5b95edc4db8b0
Nicholas Toole <nttoole@jpl.nasa.gov>
2022-06-23 16:49:40 -0700

    Initial delivery of refactored plugin.py with support for config class

--- ait/core/server/plugin.py
+++ ait/core/server/plugin.py
@@ -1,12 +1,129 @@
 from abc import ABCMeta, abstractmethod
 
+import enum
+import copy
+
+from importlib import import_module
+
 import gevent
 import gevent.monkey
 
 gevent.monkey.patch_all()
 
-import ait.core  # noqa
+from ait.core import log, cfg
 from .client import ZMQInputClient
+
+
+class Type(enum.Enum):
+    """
+    Enumeration for Plugin type: standard plugin (greenlet) or process-based
+    """
+    STANDARD = enum.auto()
+    PROCESS = enum.auto()
+
+    @classmethod
+    def value_of(cls, value, default):
+        """
+        Class method that returns a Type enum based on string value.
+        If value is None or does not match, then Type.GREENLET is returned
+        by default.
+
+        Params:
+            value: Name associated with the enum
+            default: Default value to be returned, can be None
+
+        Returns:  plugin.Type - Type enum instance
+        """
+        if value:
+            for k, v in cls.__members__.items():
+                if k.lower() == value.lower():
+                    return v
+        if default:
+            return default
+        else:
+            raise ValueError(f"'{cls.__name__}' enum not found for '{value}'")
+
+
+class PluginConfig(object):
+    """
+    Data-struct for plugin information.
+    Would be useful if we allow multiple plugins to run in a common
+    child-process
+    """
+
+    def __init__(self, name, inputs=[], outputs=[], zmq_args=None, **kwargs):
+        """
+        Constructor
+
+        Params:
+            name:       Name of the Plugin class
+            inputs:     names of inbound streams plugin receives data from
+            outputs:    names of outbound streams plugin sends its data to
+            zmq_args:   dict containing the follow keys:
+                            zmq_context
+                            zmq_proxy_xsub_url
+                            zmq_proxy_xpub_url
+                        Defaults to empty dict. Default values
+                        assigned during instantiation of parent class.
+            **kwargs:   (optional) Dependent on requirements of child class.
+        """
+        if name is None:
+            raise (cfg.AitConfigMissing("plugin name"))
+
+        self.name = name
+        self.inputs = inputs
+        self.outputs = outputs
+        self.zmq_args = zmq_args
+        self.kwargs = kwargs
+
+    # TODO: do we really need these?
+    def get_zmq_context(self):
+        return self.zmq_args.get('context', None)
+
+    def set_zmq_context(self, context):
+        self.zmq_args['context'] = context
+
+    @staticmethod
+    def build_from_config(config, zmq_args=None):
+        """
+        Static method that extracts information from AIT Plugin
+        configuration and returns a newly instantiated
+        PluginConfig object.
+        Required configuration that is missing will result in
+        error.
+
+        Params:
+            config: AIT configuration for a plugin
+            zmq_args: ZMQ settings
+
+        Returns:  PluginConfig - New PluginConfig built from config
+
+        Raises:
+            ValueError:   if any of the required config values are missing
+        """
+
+        # Make a copy of the config that we can manipulate
+        other_args = copy.deepcopy(config)
+
+        # Extract name and ensure it is defined
+        name = other_args.pop("name", None)
+        if name is None:
+            raise (cfg.AitConfigMissing("plugin name"))
+
+        plugin_inputs = other_args.pop("inputs")
+        if not plugin_inputs:
+            log.warn(f"No plugin inputs specified for {name}")
+            plugin_inputs = []
+
+        subscribers = other_args.pop("outputs")
+        if not subscribers:
+            log.warn(f"No plugin outputs specified for {name}")
+            subscribers = []
+
+        plugin_config = PluginConfig(name, plugin_inputs, subscribers,
+                                     zmq_args, other_args)
+
+        return plugin_config
 
 
 class Plugin(ZMQInputClient):
@@ -19,6 +136,8 @@ class Plugin(ZMQInputClient):
 
     def __init__(self, inputs, outputs, zmq_args=None, **kwargs):
         """
+        Constructor
+
         Params:
             inputs:     names of inbound streams plugin receives data from
             outputs:    names of outbound streams plugin sends its data to
@@ -43,7 +162,7 @@ class Plugin(ZMQInputClient):
         super(Plugin, self).__init__(**zmq_args)
 
     def __repr__(self):
-        return "<Plugin name={}>".format(self.name)
+        return f"<Plugin name={self.name}>"
 
     @abstractmethod
     def process(self, input_data, topic=None):
@@ -57,3 +176,31 @@ class Plugin(ZMQInputClient):
             topic:       Name of stream that message was received from.
         """
         pass
+
+
+    @staticmethod
+    def create_plugin(plugin_config):
+        """
+        Static utility method that instantiates extensions
+        of the Plugin class.
+
+        Params:
+            plugin_config: Plugin configuration associated with Plugin instance
+
+        Returns:  Plugin - New Plugin extension
+
+        """
+        name = plugin_config.name
+        module_name = name.rsplit(".", 1)[0]
+        class_name = name.rsplit(".", 1)[-1]
+
+        module = import_module(module_name)
+        plugin_class = getattr(module, class_name)
+        instance = plugin_class(
+            plugin_config.inputs,
+            plugin_config.outputs,
+            plugin_config.zmq_args,
+            plugin_config.other_args,
+        )
+
+        return instance
